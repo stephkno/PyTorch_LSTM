ff2911f39c14c8899d450f791c7317e3a577238d
Stephen <stephkno@gmail.com>
2019-04-14 23:38:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,109 +6,38 @@ import datetime
 from termcolor import colored
 import atexit
 import keyboard
+import random
 
 #model
-class lstm(torch.nn.Module):
+class model(torch.nn.Module):
     def __init__(self, hidden_size, rate):
-        super(lstm, self).__init__()
+        super(model, self).__init__()
 
-        self.forget_i = torch.nn.Linear(alphabet_size*sequence_length, hidden_size)
-        self.forget_h = torch.nn.Linear(hidden_size, hidden_size)
-
-        self.input_i = torch.nn.Linear(alphabet_size*sequence_length, hidden_size)
-        self.input_h = torch.nn.Linear(hidden_size, hidden_size)
-
-        self.state_i = torch.nn.Linear(alphabet_size*sequence_length, hidden_size)
-        self.state_h = torch.nn.Linear(hidden_size, hidden_size)
-
-        self.output_i = torch.nn.Linear(alphabet_size*sequence_length, hidden_size)
-        self.output_h = torch.nn.Linear(hidden_size, hidden_size)
-
-        self.reset_hidden()
-
+        self.rnn = torch.nn.LSTM(alphabet_size, hidden_size, n_layers, batch_first=True)
         self.decoder = torch.nn.Linear(hidden_size, alphabet_size)
 
         self.loss = torch.nn.BCELoss()
         print("Learning rate: ", rate)
         self.optim = torch.optim.Adam(lr=float(rate), params=self.parameters())
-
-    def reset_hidden(self):
-        self.h = torch.zeros(hidden_size).cuda()
-        self.c = torch.zeros(hidden_size).cuda()
-
-    def forward(self, input_vec):
-
-        input_vec = input_vec.detach()
-
-        h = self.h.detach()
-        c = self.c.detach()
-
-        f = torch.nn.Sigmoid()(self.forget_i(input_vec) + self.forget_h(h))
-        i = torch.nn.Sigmoid()(self.input_i(input_vec) + self.input_h(h))
-        s = torch.nn.Tanh()(self.state_i(input_vec) + self.state_h(h))
-        o = torch.nn.Sigmoid()(self.output_i(input_vec) + self.output_h(h))
-
-        c = (c * f) + (i * s)
-        h_ = torch.nn.Tanh()(c)
-        h_ = o * h_
-
-        self.c = c
-        self.h = h_
-
-        h_ = torch.nn.Dropout(0.0)(h_)
-        d = self.decoder(h_)
-        d = torch.nn.Softmax(dim=-1)(d/temperature)
-
-        return h_, d
-class gru(torch.nn.Module):
-    def __init__(self, hidden_size, rate):
-        super(lstm, self).__init__()
-
-        self.forget_i = torch.nn.Linear(alphabet_size*sequence_length, hidden_size)
-        self.forget_h = torch.nn.Linear(hidden_size, hidden_size)
-
-        self.input_i = torch.nn.Linear(alphabet_size*sequence_length, hidden_size)
-        self.input_h = torch.nn.Linear(hidden_size, hidden_size)
-
-        self.output_i = torch.nn.Linear(alphabet_size*sequence_length, hidden_size)
-        self.output_h = torch.nn.Linear(hidden_size, hidden_size)
-
         self.reset_hidden()
 
-        self.decoder = torch.nn.Linear(hidden_size, alphabet_size)
-
-        self.loss = torch.nn.BCELoss()
-        print("Learning rate: ", rate)
-        self.optim = torch.optim.Adam(lr=float(rate), params=self.parameters())
-
     def reset_hidden(self):
-        self.c = torch.zeros(hidden_size).cuda()
+        self.h = (torch.zeros(n_layers, 1, hidden_size).cuda(),
+                  torch.zeros(n_layers, 1, hidden_size).cuda())
 
     def forward(self, input_vec):
+        input_vec = input_vec.detach().unsqueeze(0)
+        context = self.h[0].detach(), self.h[1].detach()
 
-        input_vec = input_vec.detach()
+        x, context = self.rnn(input_vec, context)
 
-        context = self.c.detach()
-
-        a = torch.nn.Sigmoid()(self.forget_i(input_vec) + self.forget_h(c))
-        b = torch.nn.Sigmoid()(self.input_i(input_vec) + self.input_h(c))
-
-        o_h = context * a
-        o_i = input_vec * a
-
-        context = context * 1 - i
-
-
-        o = torch.nn.Tanh()(self.output_i(o_i) + self.output_h(o_h))
-
-
-        self.c = context
-
-        h_ = torch.nn.Dropout(0.0)(c)
+        self.h = context
+        h_ = torch.nn.Dropout(0.0)(x)
         d = self.decoder(h_)
-        d = torch.nn.Softmax(dim=-1)(d/temperature)
-
+        d = torch.nn.Softmax(dim=2)(d/temperature)
+        self.reset_hidden()
         return h_, d
+
 #load dataset
 filename = sys.argv[1]
 text = []
@@ -147,34 +76,41 @@ epochs = 0
 #parameters
 hidden_size = 512
 n_chars = 1
-batch = 1
-sequence_length = 6
+n_layers = 2
+sequence_length = 1000
 step = 0
 steps = 10000
-rate = 0.00000065
+rate = 0.0025
 c = 0
 total_loss = 0.0
 n_correct = 0
 temperature = 1.0
+epsilon = 1.0
 
 render = False
 show_grad = False
+teacher_force = True
+first = True
 
 one_hot_vecs = {}
 p_avg = 0
 avg_loss = 0
 
+for i in alphabet:
+    t = torch.zeros(alphabet_size).cuda()
+    t[alphabet.index(i.lower())] = 1.0
+    one_hot_vecs[i] = t
+
 # custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
+    if classname.find('GRU') != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
     if classname.find('Linear') != -1:
         torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
 
 def one_hot(char):
-
-    t = torch.zeros(alphabet_size).cuda()
-    t[alphabet.index(char.lower())] = 1.0
-    return t
+    return one_hot_vecs[char]
 
 def get_next_seq(character):
     c = character
@@ -184,17 +120,26 @@ def get_next_seq(character):
         c = 0
         new = True
 
-    i = 0
     out = []
+    target = []
 
     for i in range(sequence_length):
         char = one_hot(text[(c+i)%len(text)].lower())
         out.append(char)
+    for i in range(sequence_length):
+        char = one_hot(text[(c+i+1)%len(text)].lower())
+        target.append(char)
+    """
+    print("\n")
+    for o in out:
+        print(alphabet[torch.argmax(o)],end="")
+    print("\n")
+    for o in target:
+        print(alphabet[torch.argmax(o)],end="")
+    print("\n-\n")
+    """
+    return out, torch.stack(target).cuda(), c+sequence_length+1, new
 
-    i+=1
-    target = one_hot(text[(c+i)%len(text)].lower()).cuda()
-
-    return out, target, c+1, new
 
 def savemodel():
     print("Save model parameters? [y/n]➡")
@@ -223,7 +168,7 @@ def savemodel():
 atexit.register(savemodel)
 
 #init model
-model = lstm(hidden_size, rate).cuda()
+model = model(hidden_size, rate).cuda()
 weights_init(model)
 
 counter = 1
@@ -240,84 +185,98 @@ print(" - Size:{}\n".format(alphabet_size))
 
 #train loop
 while True:
-
-    inp = torch.zeros(alphabet_size)
+    if False:
+        for p in model.parameters():
+            print(p)
+            print(p.grad)
+    elif keyboard.is_pressed(' '):
+        teacher_force = not teacher_force
+        if teacher_force:
+            print("\nForce on")
+        else:
+            print("\nForce off")
+        print("")
 
     inp, target, c, new = get_next_seq(c)
 
     a = 0
 
     if new:
-        if epochs % 100 == 0:
+        model.reset_hidden()
+        if epochs % 1 == 0 and not render:
             print("")
-            for i in range(int(len(out_text)/4)):
+            for i in range(int(len(out_text))):
                 print(out_text[i],end="")
             print("")
 
         out_text.clear()
         avg_loss = round(total_loss / len(text), 4)
+        accuracy = int(100 * (n_correct / counter))
+
+        out = colored(
+            "[Epoch{}|Progress:[{}%]|[{}]|loss:{}|avg:{}|{}|Acc:{}%|Eps:{}]".format(epochs, progress, a, total_loss,
+                                                                                    avg_loss, indicator, n_correct, epsilon), attrs=['reverse'])
         n_correct = 0
         counter = 0
-        model.reset_hidden()
+        total_loss = 0
         epochs += 1
+        first = True
 
-    d, out = model.forward(torch.cat(inp))
+    inp = torch.stack(inp)
 
-    probs = torch.distributions.Categorical(out)
-    a = probs.sample()
+    d, out = model.forward(inp)
 
-    outchar = alphabet[a]
-    targetchar = alphabet[torch.argmax(target)]
+    probs = torch.distributions.Categorical(out.squeeze(0))
+    sample = probs.sample().cuda()
+
+    outchar = [alphabet[s] for s in sample]
+    targetchar = [alphabet[torch.argmax(t)] for t in target]
+
+    if not teacher_force:
+        next_inp = []
+        for o in outchar:
+            next_inp.append(one_hot(o))
 
     done = False
 
-    if outchar == targetchar:
-        a = "✅"
-        n_correct += 1
-        done = True
-    else:
-        a = "❌"
-
-    loss = model.loss(out.view(-1), target.view(-1))
+    loss = model.loss(out, target.unsqueeze(0))
     loss.backward(retain_graph=True)
-    #torch.nn.utils.clip_grad_norm_(model.parameters(), -1.0, 1.0)
 
-    if keyboard.is_pressed(' '):
-        show_grad = True
-
-    if show_grad:
-        for p in model.parameters():
-            print(p)
-            print(p.grad)
-            break
-        show_grad = False
+    total_loss += loss.item()
+    #torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
 
     model.optim.step()
     model.optim.zero_grad()
 
     counter += 1
 
-    accuracy = int(100*(n_correct/counter))
     p_avg = avg_loss
     if avg_loss > p_avg:
         indicator = "⬆"
     else:
         indicator = "⬇"
 
-    out_text.append(outchar)
+    for o in outchar:
+        out_text.append(o)
 
-    progress = round(100*(c/len(text)),4)
+    progress = round(100*(c/len(text)),1)
+    if not render and step % steps == 0:
+        print("\r", progress, end="%")
 
     if targetchar == "¶":
         sys.stdout.flush()
-        model.reset_hidden()
 
-    if not render:
-        if counter % 100 == 0:
-            out = colored("\r[Epoch{}|Progress:[{}%]|[{}]|loss:{}|avg:{}|{}|Acc:{}%]".format(epochs, progress, a, 0, avg_loss, indicator, accuracy),attrs=['reverse'])
-            print(out,end="")
+    if render:
+        #if counter % 10 == 0:
+        #    out = colored("\r[Epoch{}|Progress:[{}%]|[{}]|loss:{}|avg:{}|{}|Acc:{}%]".format(epochs, progress, a, 0, avg_loss, indicator, accuracy),attrs=['reverse'])
+        #    print(out,end="")
+        #    sys.stdout.flush()
+
+        for i in outchar:
+            if i == "¶":
+                i = "\n"
+            print(i, end="")
+            sys.stdout.flush()
     else:
-        print(outchar,end="")
-
-
-    del out, inp, target, outchar, targetchar
+        print("\r",progress,end="%")
+    torch.cuda.empty_cache()
